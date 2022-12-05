4f0d16109904bcbe80242b1f51ff72c516e4d488
argonautica <sidplusk@yahoo.com>
2022-12-05 15:41:37 -0800

    remove comments

--- src/scriptv1.js
+++ src/scriptv1.js
@@ -4,13 +4,8 @@ window.addEventListener('DOMContentLoaded', init);
 
 const items = document.getElementsByClassName("list-item");
 const deleteButtons = document.getElementsByClassName("delete");
-/**
- * Populate the document with items.
- */
 
-// Global variables (items) cannot be used in init() function
 function init() {
-    // *** ADD ITEM BUTTON ***
     const addButton = document.getElementsByClassName('item-add-btn')[0];
     addButton.addEventListener('click', function() {
         addItem("", false, "");
@@ -21,18 +16,10 @@ function init() {
     for (let i = 0; i < items.length; i++) {
         addItem(items[i][1], items[i][0], "");
     }
-    // Add delete functionality to existing items on load
     window.addEventListener('load', addDeleteEventListnerOnLoad());
 }
 
-/**
- * Add items to the document
- * @param {string} name - The name of the item.
- * @param {boolean} checked - The status of the item.
- * @param {string} category - The category of the item.
- */
 const addItem = (name, checked, category) => {
-    // console.log(items);
     const listDOM = document.getElementsByClassName('list')[0];
     const newItem = document.createElement('div');
     newItem.className = 'list-item';
@@ -51,15 +38,10 @@ const addItem = (name, checked, category) => {
   `
     newItem.getElementsByClassName("list-content")[0].getElementsByTagName("input")[0].value = name;
     newItem.getElementsByTagName("label")[0].getElementsByTagName("input")[0].checked = checked;
-    // console.log(newItem.getElementsByClassName("list-content"));
     listDOM.appendChild(newItem);
 }
 
-/**
- * Add delete functionality to each item.
- */
 function addDeleteEventListnerOnLoad() {
-  // let deleteButtons = document.getElementsByClassName("delete");
   for(let i = 0; i < deleteButtons.length; i++){
       console.log("inside the array");
       deleteButtons[i].addEventListener("click", (e) => {
@@ -72,7 +54,6 @@ function addDeleteEventListnerOnLoad() {
 
 function addDeleteEventListener() {
     const listDOM = document.getElementsByClassName("list-item");
-    // console.log(`just added a new item, wanted to make sure the length of list is correct: ${items.length}`);
     let endIndex = deleteButtons.length-1;
     deleteButtons[endIndex].addEventListener("click", (e) => {
         const index = Array.from(deleteButtons).indexOf(e.target);
@@ -84,9 +65,6 @@ const removeItem = (index) => {
     items[index].remove();
 }
 
-/**
- * Update the status of all items to "uncheck".
- */
 function uncheckAll() {
     let checkBoxes = document.querySelectorAll("input[type=checkbox]");
     checkBoxes.forEach(item => {
@@ -94,11 +72,7 @@ function uncheckAll() {
     });
 }
 
-/**
- * Manually save all items to localStorage.
- */
 function save() {
-    // let items = document.getElementsByClassName("list-item");
     let store = [];
     for (let i = 0; i < items.length; i++) {
         let a = [items[i].getElementsByTagName("label")[0].getElementsByTagName("input")[0].checked, items[i].getElementsByClassName("list-content")[0].getElementsByTagName("input")[0].value];
@@ -108,12 +82,8 @@ function save() {
     localStorage.setItem("storage", JSON.stringify(store));
 }
 
-/**
- * Save all items to localStorage before closing homepage.
- */
 window.onbeforeunload = confirmExit;
 function confirmExit() {
-    // let items = document.getElementsByClassName("list-item");
     console.log(items);
     let store = [];
     for (let i = 0; i < items.length; i++) {
@@ -125,7 +95,4 @@ function confirmExit() {
     return false;
 }
 
-// module.exports = items;
-// module.exports = deleteButtons;
-// module.exports = addItem();
 module.exports = { addItem, removeItem, uncheckAll, items};
